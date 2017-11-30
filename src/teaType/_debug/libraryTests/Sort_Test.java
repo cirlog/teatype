ddbd00043ec91dba511d08044ef073018c4651a7
arsonite <tr_burak@hotmail.de>
2017-11-30 14:48:10 +0100

    refractoring

--- src/teaType/_debug/libraryTests/Sort_Test.java
+++ src/teaType/_debug/libraryTests/Sort_Test.java
@@ -2,13 +2,12 @@ package teaType._debug.libraryTests;
 
 import teaType.util.Array
 ;
-import teaType.util.statics.Print;
 import teaType.util.statics.Randomizer;
 
 public class Sort_Test {
 	public static void main(String[] args) {
 		int[] arr = Randomizer.generateInteger(20, 1000, false, false, false);
 		Array.quicksort(arr, true);
-		Print.integerArray(arr, true, true);
+		Array.print(arr, true, true);
 	}
 }
