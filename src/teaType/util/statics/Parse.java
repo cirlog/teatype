36d20793ba2de835c57713dbc9395ba30f413801
arsonite <tr_burak@hotmail.de>
2017-11-30 14:47:22 +0100

    modified statics.Parse

--- src/teaType/util/statics/Parse.java
+++ src/teaType/util/statics/Parse.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Parse {
 	static String userInput;
 	static Scanner in;
-	
+
 	public static String parseUserInput() {
 		in = new Scanner(System.in);
 		return userInput = in.next();
 	}
-	
+
 	public static String toString(int i) { return String.valueOf(i); }
-	
+
 	public static String toString(double d) { return String.valueOf(d); }
-	
+
 	public static String toString(long l) { return String.valueOf(l); }
-	
+
 	public static String toString(float f) { return String.valueOf(f); }
-	
+
 	public static String toString(boolean b) { return String.valueOf(b); }
-	
+
 	public static int toInteger(String s) {
 		in = new Scanner(s);
 		if(in.hasNextInt()) {
@@ -28,7 +28,14 @@ public class Parse {
 		}
 		return 0;
 	}
-	
+
 	public static int toInteger(double d) { return (int) (d); }
-	
+
+	public static int toInteger(boolean b) {
+		if(b) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 }
