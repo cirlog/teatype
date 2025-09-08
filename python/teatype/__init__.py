d14359cb23681974eec8d66a996cb9ae06d4e7ab
glurakguenaydin <burak.guenaydin@enamentis.de>
2025-09-08 17:12:37 +0200

    feat: Update print function signature and enhance print functionality for better output handling

--- python/teatype/__init__.py
+++ python/teatype/__init__.py
@@ -10,4 +10,5 @@
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 
+from .io.print import print
 from .util import *
